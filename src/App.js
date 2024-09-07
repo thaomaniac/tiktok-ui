27dd40457d654366029208bd568c2bbbdb947a22
Vu Thanh Thao <thaomaniac@gmail.com>
2024-09-07 16:57:31 +0700

    master: Router, layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import {Fragment} from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {publicRoutes} from "~/routes";
+import {DefaultLayout} from "~/components/Layout";
+
 function App() {
-    return (
-        <div className="App">
-            <h1> React</h1>
-        </div>
-    );
+    return (<div className="App">
+        <Router>
+            <Routes>
+                {publicRoutes.map((route, index) => {
+                    const Layout = route.layout ?? (route.layout === null ? Fragment : DefaultLayout);
+                    const Page = route.component;
+                    return <Route key={index} path={route.path} element={<Layout><Page/></Layout>}/>;
+                })}
+            </Routes>
+        </Router>
+    </div>);
 }
 
 export default App;
---
+++ src/pages/Search/index.js
@@ -0,0 +1,4 @@
+function Search() {
+    return <h2>Search</h2>
+}
+export default Search;
